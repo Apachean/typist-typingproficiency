d3c38e7b30f1d0a0dde18b2c0533ea241c8d73c5
codingmr <codingmroberts@gmail.com>
2014-07-14 04:26:28 +0100

    Connection to MySQL
    Untested

--- Typist/Accounts.cs
+++ Typist/Accounts.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MetroFramework;
+using MySql.Data.MySqlClient;
 
 namespace Typist
 {
@@ -33,6 +34,47 @@ namespace Typist
         {
             login.BringToFront();
             register.SendToBack();
+        }
+
+        private void createUser_AsROOT(string user, string pass)
+        {
+            string connstr = "Server=localhost;user=root;port3306;password=homebase;";
+            using (var conn = new MySqlConnection(connstr))
+            using (var cmd = conn.CreateCommand())
+            {
+                // open, 
+                conn.Open();
+                // Set the authentication to sha256 passwords
+                cmd.CommandText = "CREATE USER '" + user + "'@'localhost' IDENTIFIED BY sha256_password;";
+                cmd.CommandText = "SET old_passwords = 2;";
+                // Set password
+                cmd.CommandText = "SET PASSWORD FOR '" + user + "'@'localhost' = PASSWORD('" + pass + "');";
+
+                // Create user's table data database
+                cmd.CommandText = "CREATE DATABASE IF NOT EXISTS '" + user + "_db'";
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    // User already exists?
+                    throw;
+                }
+            }
+        }
+
+        private void connectMySQL_AsUSER(string user, string pass)
+        {
+            string connstr = "Server=localhost;Database=" + user + "_db;" + "user=" + user + ";port3306;password=" + pass + ";";
+            using (var conn = new MySqlConnection(connstr))
+            using (var cmd = conn.CreateCommand())
+            {
+                conn.Open();
+
+            }
+
         }
     }
 }
